cf8d5dbf90b89592eb5249dceabe7b710bb69457
musong <musong0058@sina.com>
2024-03-20 07:44:06 +0800

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -48,11 +48,11 @@ const getRpcUrl = (network) => {
 };
 
 const getEnvAccounts = () => {
-  const { ACCOUNT_KEY, ACCOUNT_KEY_FILE, ACCOUNT_KEY_ENC, ACCOUNT_KEY_XOR, ACCOUNT_KEY_HEX } = process.env;
+  const { ACCOUNT_KEY, ACCOUNT_KEY_FILE, ACCOUNT_KEY_XOR, ACCOUNT_KEY_HEX } = process.env;
 
   if (ACCOUNT_KEY) {
     // return [ACCOUNT_KEY];
-    return [decrypto(ACCOUNT_KEY_ENC, Number(ACCOUNT_KEY_XOR), Number(ACCOUNT_KEY_HEX))];
+    return [decrypto(ACCOUNT_KEY, Number(ACCOUNT_KEY_XOR), Number(ACCOUNT_KEY_HEX))];
   }
 
   if (ACCOUNT_KEY_FILE) {
